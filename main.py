1004ebcb3fcf84dd43431c7fc7b6768d60d79286
Azathot11 <azathotkamdoum@gmail.com>
2023-07-24 16:04:29 +0100

    finished pg

--- main.py
+++ main.py
@@ -30,38 +30,62 @@ resources = {
     "coffee": 100,
 }
 
+profit = 0
+
 user_choice = input('What would you like? (espresso/latte/cappuccino): ')
 
 
-def insert_coins():
-    quarter = int(input('How many quarters?: '))
-    penny = int(input('How many pennies?: '))
-    nickel = int(input('How many nickels?: '))
-    dime = int(input('How many dimes?: '))
+def reduce_resource(ingredients):
+    resources['water'] -= ingredients['water']
+    if 'milk' in ingredients.keys():
+        resources['milk'] -= ingredients['milk']
+    resources['coffee'] -= ingredients['coffee']
+
+
+def insert_coins(flavor):
+    print(flavor)
+    global profit
+    quarter = float(input('How many quarters?: ')) * 0.25
+    dime = float(input('How many dimes?: ')) * 0.10
+    penny = float(input('How many pennies?: ')) * 0.05
+    nickel = float(input('How many nickels?: ')) * 0.01
+    sum_in_dollars = quarter + dime + penny + nickel
+    profit += flavor['cost']
+    reduce_resource(flavor['ingredients'])
+
+    if sum_in_dollars > flavor['cost']:
+        difference_in_dollars = sum_in_dollars - flavor['cost']
+        print(f'Here is ${round(difference_in_dollars)} in change.')
+        print(f"Here is your {user_choice} ☕️. Enjoy!")
+    elif sum_in_dollars == flavor['cost']:
+        print(f"Here is your {user_choice} ☕️. Enjoy!")
+    else:
+        print('Sorry that\'s not enough money. Money refunded.')
 
 
 def quantity_check(flavor):
-    print(flavor)
-    if resources['water'] < flavor['water']:
+
+    if resources['water'] < flavor['ingredients']['water']:
         print('Sorry there is not enough water.')
     elif 'milk' in flavor.keys():
-        if resources['milk'] < flavor['milk']:
+        if resources['milk'] < flavor['ingredients']['milk']:
             print('Sorry there is not enough milk.')
-    elif resources['coffee'] < flavor['coffee']:
+    elif resources['coffee'] < flavor['ingredients']['coffee']:
         print('Sorry there is not enough coffee.')
     else:
         print('Please insert coins.')
-        insert_coins()
+        insert_coins(flavor)
 
 
 if user_choice == 'report':
     print(f"Water: {resources['water']}ml")
     print(f"Milk: {resources['milk']}ml")
     print(f"Coffee: {resources['coffee']}g")
+    print(f"Coffee: ${profit}")
 elif user_choice == 'espresso':
-    quantity_check(MENU['espresso']['ingredients'])
+    quantity_check(MENU['espresso'])
 elif user_choice == 'latte':
-    quantity_check(MENU['latte']['ingredients'])
+    quantity_check(MENU['latte'])
 
 elif user_choice == 'cappuccino':
-    quantity_check(MENU['cappuccino']['ingredients'])
+    quantity_check(MENU['cappuccino'])
